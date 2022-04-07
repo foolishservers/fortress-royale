ec6704c892d732c77b81e06b8584787943e54785
gaejuck <foolish.server@gmail.com>
2022-04-07 21:49:37 +0900

    oops6

--- scripts/package.sh
+++ scripts/package.sh
@@ -1,3 +1,18 @@
 cd build
 
-zip -r package.zip addons/sourcemod/plugins/royale.smx ../addons/sourcemod/configs/royale/ ../addons/sourcemod/gamedata/royale.txt ../addons/sourcemod/translations/ ../materials/ ../models/ ../sound/ ../scripts/vehicles/
+mkdir -p package/addons/sourcemod/plugins
+mkdir -p package/addons/sourcemod/gamedata
+mkdir -p package/addons/sourcemod/configs
+
+cp -r addons/sourcemod/plugins/royale.smx package/addons/sourcemod/plugins
+cp -r ../addons/sourcemod/gamedata/royale.txt package/addons/sourcemod/gamedata
+cp -r ../addons/sourcemod/configs/royale package/addons/sourcemod/configs
+cp -r ../addons/sourcemod/translations package/addons/sourcemod
+cp -r ../materials package
+cp -r ../models package
+cp -r ../sound package
+cp -r ../scripts/vehicles package/scripts
+
+cd package
+
+zip package.zip -r ./*
